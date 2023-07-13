6108fcdebd79228ebf38ab81755a8b1856a6d1b9
Alexey Filimonov <filimonov.alks@gmail.com>
2023-07-14 00:09:57 +0300

    plant timber

---
+++ app/src/main/kotlin/ru/stolexiy/openweather/Application.kt
@@ -0,0 +1,28 @@
+package ru.stolexiy.openweather
+
+import android.app.Application
+import android.os.Process
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class Application : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(object : Timber.DebugTree() {
+                override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+                    super.log(
+                        priority, "[$GLOBAL_TAG] " +
+                                "$tag | ${Process.getElapsedCpuTime()} ms", message, t
+                    )
+                }
+            })
+        }
+    }
+
+    companion object {
+        private const val GLOBAL_TAG: String = "OW"
+    }
+}
